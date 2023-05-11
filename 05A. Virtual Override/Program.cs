dfb19ce6de88454e99bf10ac308e21a8134d425d
kinara <kinara.varga@outlook.com>
2023-05-11 14:00:43 +0700

    Co-authored-by: zulfafani <zulfafani@users.noreply.github.com> Co-authored-by: d00h4n <d00h4n@users.noreply.github.com> Co-authored-by: Angga Prasetyo <manggaperas@users.noreply.github.com> Co-authored-by: shaella <shaella@users.noreply.github.com> Co-authored-by: Friday6661 <Friday6661@users.noreply.github.com> Co-authored-by: teguh riyadi <teguhriyadi222@users.noreply.github.com> Co-authored-by: adam ardiyanto <adamardiyanto@users.noreply.github.com> Co-authored-by: Fariq <Fariq01@users.noreply.github.com>

---
+++ 05A. Virtual Override/Program.cs
@@ -0,0 +1,35 @@
+﻿class Parent
+{
+	public Parent()
+	{
+		Console.WriteLine("Parent constructor");
+	}
+	public virtual void MethodA()
+	{
+		Console.WriteLine("Method Parent");
+	}
+}
+class Child : Parent
+{
+	public Child()
+	{
+		Console.WriteLine("Child constructor");
+	}
+	public override void MethodA()
+	{
+		Console.WriteLine("Method Child");
+	}
+}
+class Program
+{
+	static void Main()
+	{
+		Child child = new Child();
+		Parent parent = new Parent();
+		child.MethodA();
+		parent.MethodA();
+
+		Parent parent2 = child;
+		parent2.MethodA();
+	}
+}
---
+++ Namespaces/Library.cs
@@ -0,0 +1,9 @@
+namespace CalculatorLibrary;
+
+static class Calculator 
+{
+	public static int Add(int a, int b) 
+	{
+		return a + b;
+	}
+}
